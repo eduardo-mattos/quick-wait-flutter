61417e49a594614d7aeb9ae89c5de936dd09c154
Matheus Souto <matheushsouto@gmail.com>
2022-02-04 09:40:20 -0300

    Atualizando branch

--- lib/features/find_hospital/presentation/pages/hospital_page.dart
+++ lib/features/find_hospital/presentation/pages/hospital_page.dart
@@ -1,14 +1,15 @@
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:http/http.dart';
 import 'package:latlong2/latlong.dart' as latLng;
-import 'package:geolocator/geolocator.dart'
-    as geolocator; // or whatever name you want
+import 'package:geolocator/geolocator.dart' as geolocator; // or whatever name you want
 import 'package:http/http.dart' as http;
+import 'package:quick_wait_android/design_system/buttons/rounded_icon_button.dart';
+import 'package:quick_wait_android/features/home/presentation/widgets/home_large_button.dart';
 
 class HospitalPage extends StatefulWidget {
   HospitalPage() : super();
@@ -19,9 +20,8 @@ class HospitalPage extends StatefulWidget {
 class _HospitalPageState extends State<HospitalPage> {
   double latitude = 0;
   double longitude = 0;
-
   MapController controller = new MapController();
-  @override
+  
   void initState() {
     getUserLocation();
     super.initState();
@@ -33,89 +33,122 @@ class _HospitalPageState extends State<HospitalPage> {
   }
 
   Future getHospitalsLocation() async {
+    var longitudeMin = longitude - 0.01;
+    var latitudeMin = latitude - 0.01;
+    var longitudeMax = longitude + 0.01;
+    var latitudeMax = latitude + 0.01;
+
     var baseUrl =
         'https://api.mapbox.com/geocoding/v5/mapbox.places/Hospital%2Cupa.json?';
-    final uri = Uri.parse('$baseUrl').replace(queryParameters: {
-      'limit': '10',
-      'language': 'pt-PT',
-      'access_token':
-          'pk.eyJ1IjoibWF0aGV1c2hzb3V0byIsImEiOiJja3ozMTFyd2wwMjk3MzBtOGRvdG8wdXR0In0.5ZhExvzt7Xe0A37HsBLtUw'
-    });
+
+    var bbox = 'bbox=${longitudeMin}%2C${latitudeMin}%2C${longitudeMax}%2C${latitudeMax}';
+    var token = 'pk.eyJ1IjoibWF0aGV1c2hzb3V0byIsImEiOiJja3ozMTFyd2wwMjk3MzBtOGRvdG8wdXR0In0.5ZhExvzt7Xe0A37HsBLtUw';
+    final uri = Uri.parse('$baseUrl' + bbox + '&limit=10' + '&language=pt-PT' + '&access_token=' + token);
     var response = await http.get(uri);
-    // print(response.statusCode);
-    // print(json.decode(response.body));
+    print(json.decode(response.body));
     return response;
   }
 
   getUserLocation() async {
     var currentLocation = await locateUser();
-    var response = await getHospitalsLocation();
 
-    var a = LatLngBounds(latLng.LatLng(currentLocation.latitude, currentLocation.longitude));
-    print(currentLocation);
-    print("Entrou aqui");
-    print(a.southEast);
-    print("Entrou aqui 2");
-    print(a.southWest);
     setState(() {
       latitude = currentLocation.latitude;
       longitude = currentLocation.longitude;
+
+      var response = getHospitalsLocation();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: latitude == 0
-          ? Container()
-          : FlutterMap(
-              options: MapOptions(
-                center: latLng.LatLng(latitude, longitude),
-                zoom: 13.0,
-                containPoint: (){
-
-                }
-              ),
-              layers: [
-                TileLayerOptions(
-                    urlTemplate:
-                        "https://api.mapbox.com/styles/v1/matheushsouto/ckz31yo9s000o16n6wtlzgitt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWF0aGV1c2hzb3V0byIsImEiOiJja3ozMTFyd2wwMjk3MzBtOGRvdG8wdXR0In0.5ZhExvzt7Xe0A37HsBLtUw",
-                    additionalOptions: {
-                      'accessToken':
-                          'pk.eyJ1IjoibWF0aGV1c2hzb3V0byIsImEiOiJja3ozMTFyd2wwMjk3MzBtOGRvdG8wdXR0In0.5ZhExvzt7Xe0A37HsBLtUw',
-                      'id': 'mapbox.mapbox-streets-v8'
-                    }),
-                MarkerLayerOptions(
-                  markers: [
-                    Marker(
-                        width: 80.0,
-                        height: 80.0,
-                        point: latLng.LatLng(latitude, longitude),
-                        builder: (context) => IconButton(
-                              icon: const Icon(Icons.location_on),
-                              color: Colors.red,
-                              iconSize: 45.0,
-                              onPressed: () {
-                                getUserLocation();
-                              },
-                      )),
-                  ],
+      resizeToAvoidBottomInset: false,
+      body: Stack(children: [
+        latitude == 0
+            ? Container()
+            : FlutterMap(
+                options: MapOptions(
+                  center: latLng.LatLng(latitude, longitude),
+                  zoom: 13.0,
                 ),
-                CircleLayerOptions(circles: [
-                  CircleMarker(
-                      //radius marker
-                      point: latLng.LatLng(latitude, longitude),
-                      color: Colors.blue.withOpacity(0.3),
-                      borderStrokeWidth: 3.0,
-                      borderColor: Colors.blue,
-                      radius: 100,
-                      useRadiusInMeter: true
-                       //radius
-                      )
-                ],  // circles: circles,
-              )
-              ],
-            ),
+                layers: [
+                  TileLayerOptions(
+                      urlTemplate:
+                          "https://api.mapbox.com/styles/v1/matheushsouto/ckz31yo9s000o16n6wtlzgitt/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibWF0aGV1c2hzb3V0byIsImEiOiJja3ozMTFyd2wwMjk3MzBtOGRvdG8wdXR0In0.5ZhExvzt7Xe0A37HsBLtUw",
+                      additionalOptions: {
+                        'accessToken':
+                            'pk.eyJ1IjoibWF0aGV1c2hzb3V0byIsImEiOiJja3ozMTFyd2wwMjk3MzBtOGRvdG8wdXR0In0.5ZhExvzt7Xe0A37HsBLtUw',
+                        'id': 'mapbox.mapbox-streets-v8'
+                      }),
+                  MarkerLayerOptions(
+                    markers: [
+                      Marker(
+                          width: 80.0,
+                          height: 80.0,
+                          point: latLng.LatLng(latitude, longitude),
+                          builder: (context) => IconButton(
+                                icon: const Icon(Icons.location_on),
+                                color: Colors.red,
+                                iconSize: 45.0,
+                                onPressed: () {
+                                  print("Entrou aqui");
+                                  showModalBottomSheet(
+                                      context: context,
+                                      builder: (builder) {
+                                        return Container(
+                                          color: HexColor("#E4FDFF"),
+                                          child: Center(
+                                            child: Column(
+                                              children: [
+                                                const SizedBox(height: 20),
+                                                HomeLargeButton(
+                                                  height: 50,
+                                                  onPressed: () {},
+                                                  title: "UPA 24h",
+                                                  icon: Image.asset(
+                                                    "assets/icons/hospitals.png",
+                                                  ),
+                                                ),
+                                                SizedBox(height: 15),
+                                                HomeLargeButton(
+                                                  height: 50,
+                                                  onPressed: () => null,
+                                                  title: "UPA 24h",
+                                                  icon: Image.asset(
+                                                    "assets/icons/hospitals.png",
+                                                  ),
+                                                ),
+                                                SizedBox(height: 15),
+                                                HomeLargeButton(
+                                                  height: 50,
+                                                  onPressed: () => null,
+                                                  title: "UPA 24h",
+                                                  icon: Image.asset(
+                                                    "assets/icons/hospitals.png",
+                                                  ),
+                                                ),
+                                                SizedBox(height: 15),
+                                                HomeLargeButton(
+                                                  height: 50,
+                                                  title: "UPA 24h",
+                                                  icon: Image.asset(
+                                                    "assets/icons/hospitals.png",
+                                                  ),
+                                                  onPressed: () => null,
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        );
+                                      });
+                                },
+                              )),
+                    ],
+                  ),
+                ],
+              ),
+      ]),
     );
   }
 }
