abf21d8909b9941c1d85c7e1304c898065f9c71f
emattos <emattos@lideraviacao.com.br>
2022-04-25 14:15:15 -0300

    HttpClient error message fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,32 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'app_module.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:flutter/foundation.dart';
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
 
 Future<void> main() async {
+  //TODO: remove HttpOverrides on production
+  HttpOverrides.global = MyHttpOverrides();
+
   await SentryFlutter.init(
     (options) {
-      options.dsn =
-          'https://7a4642b56784428bb60a265f7e9526ba@o1144940.ingest.sentry.io/6209545';
+      options.dsn = 'https://7a4642b56784428bb60a265f7e9526ba@o1144940.ingest.sentry.io/6209545';
       options.tracesSampleRate = 1.0;
       options.tracesSampler = (samplingContext) {
         // return a number between 0 and 1 or null (to fallback to configured value)
       };
     },
-    appRunner: () =>
-        runApp(ModularApp(module: AppModule(), child: const AppWidget())),
+    appRunner: () => runApp(ModularApp(module: AppModule(), child: const AppWidget())),
   );
 
   //  initializeDateFormatting().then((_) => runApp(ModularApp(module: AppModule(), child: const AppWidget())));
@@ -34,12 +45,8 @@ class _AppWidgetState extends State<AppWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quick Wait',
-      theme: ThemeData(
-        fontFamily: 'Quicksand'
-      ),
-      navigatorObservers: [
-        SentryNavigatorObserver()
-     ],
+      theme: ThemeData(fontFamily: 'Quicksand'),
+      navigatorObservers: [SentryNavigatorObserver()],
     ).modular();
   }
 }
